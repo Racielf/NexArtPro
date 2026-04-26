208e650604c808ad8b96cc3dbbacfd50d92cd3a8
Racielf <racinllerf@gmail.com>
2026-04-26 14:00:58 -0700

    Pass selected attachment IDs into estimate send flow

--- src/components/estimates/EstimateSendReview.jsx
+++ src/components/estimates/EstimateSendReview.jsx
@@ -278,6 +278,7 @@ export default function EstimateSendReview({ estimate, open, onClose, onSent, on
         message,
         currentTemplate,
         currentOptions,
+        includedAttachmentIds,
         appConfig,
       });
 
